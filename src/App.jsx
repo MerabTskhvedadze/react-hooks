296b13477dc2fc48d5fb4ae894d46b9b2573c654
MerabTskhvedadze <merabi.tskhvedadze@gmail.com>
2023-03-24 17:55:26 +0400

    use-effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,57 @@
-import { useState } from 'react';
-import UseState from './UseState';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <>
-      <UseState />
-    </>
-  );
+  /*
+    //note: this is most basic use case of useEffect
+    const [resourceType, setResourceType] = useState('posts');
+    const [items, setItems] = useState([]);
+    
+    useEffect(() => {
+      fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((res) => res.json())
+      .then((json) => setItems(json));
+    }, [resourceType]);
+    //note: without second argument useEffect will run on every re-render of component
+    //note: if we have second argument but it is empty array useEffect will run onMount
+    //note: but if we have some state variable(like that) in array it will run on every change of this state
+
+    
+    return (
+      <>
+        <div>
+          <button onClick={() => setResourceType('posts')}>Posts</button>
+          <button onClick={() => setResourceType('users')}>Users</button>
+          <button onClick={() => setResourceType('comments')}>Comments</button>
+        </div>
+        <h1>{resourceType}</h1>
+        {items.map((item) => {
+          return <p>{JSON.stringify(item)}</p>;
+        })}
+      </>
+    );
+  */
+
+  //note: complicated example
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = (e) => setWindowWidth(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    //note: cleanup function
+    //note: cleanup function runs first to cle  an up code that we write last time
+    //note: also cleanup function runs when component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  //note: when we add eventListener if we for example delete or unMount App component we want to
+  //note: remove this eventListener because otherwise we gonna re-add event listeners
+  //note: which is gonna slow down performance
+  //note: so we writting cleanup functions that cleaningup code that we write in useEffect
+
+  return <h1>width: {windowWidth}</h1>;
 }
 
 export default App;
-
-//**React hook rules**//
-/* 
-~1)Only call Hooks at the top level: 
-  You should only call Hooks from the top level of your component
-  or your own custom Hooks. 
-  You should not call Hooks inside loops, conditions, or nested functions.
-
-~2)Only call Hooks from React function components:
-  You can only call Hooks from React function components,
-  not from regular JavaScript functions.
-
-~3)Use the useEffect Hook for side effects: 
-  You should use the useEffect Hook for any logic that runs after render, 
-  such as setting up a subscription or changing the document title.
-
-~4)Keep state updates simple: 
-  State updates should be simple and not depend on previous state values. 
-  If you need to update state based on the previous state, you should use the setState callback instead of updating state directly.
-
-~5)Don't call Hooks inside loops: 
-  Hooks should not be called inside loops 
-  because they will cause multiple re-renders 
-  and potentially unexpected behavior.
-
-~6)Don't call Hooks from inside React class components: 
-  You should not call Hooks from inside React 
-  class components because Hooks are 
-  only meant to be used with function components.
-*/

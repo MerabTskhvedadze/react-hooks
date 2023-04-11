5b1c7956c37ecec327fcedaa0f1510c6e5a442f2
MerabTskhvedadze <merabi.tskhvedadze@gmail.com>
2023-04-11 22:59:21 +0400

    use-ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,24 @@
-import { useState, useMemo } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export default function App() {
-  //note: every time our component re-renders this sloFunction runs
-  //note: and it causes a delay on any changes that causes the components re-render
-  //note: solution in this situation is useMemo hook (memo stands for memoization)
-  //note: useMemo caching a value so you dont have to recompute it every single time
-  const [number, setNumber] = useState(0);
-  const [dark, setDark] = useState(false);
-  //note: slowFunction runs if number changes
-  //note: like this we have delay when numbers calculation is needed but not every single time
-  //note: when some state changes for (example when themes state changes)
-  const doubleNumber = useMemo(() => slowFunction(number), [number]);
+  const [name, setName] = useState('');
+  const renderCount = useRef(0);
 
-  const themeStyles = {
-    backgroundColor: `${dark ? 'black' : 'white'}`,
-    color: `${dark ? 'white' : 'black'}`,
-  };
-
-  //note: useMemo also saving the value, the previous value in some memory variable
-  //note: and this memory getting larger every time when we using memo
-  //note: so if we use useMemo every time when we actualy do not need
-  //note: it may cause performance issues
+  //note: ref does not causes reupdate component when it is changed
+  //note: so if we want to count renders we should use ref
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+  });
 
   return (
-    <>
+    <div>
       <input
-        type='number'
-        value={number}
-        onChange={(e) => setNumber(parseInt(e.target.value))}
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
-      <button onClick={() => setDark((prev) => !prev)}>Change themes</button>
-      <div style={themeStyles}>{doubleNumber}</div>
-    </>
+      <h1>My Is Name {name}</h1>
+      <h2>i rendered {renderCount.current} times</h2>
+    </div>
   );
-}
-
-function slowFunction(num) {
-  console.log('calling slow function');
-  for (let i = 0; i < 1000000000; i++) {}
-  return num * 2;
 }
